54dbbfc770a4307179b6c50e7239beaa9c77e9b6
Tanyrus <unbridledscholar@gmail.com>
2022-07-26 14:30:51 -0700

    Push to work from desktop

--- versionCheck.cs
+++ versionCheck.cs
@@ -18,10 +18,25 @@ namespace TR.Tools {
     public class versionCheck {
 
         public static string currentGameVersion;
+        public static bool isModVersionChecked;
+        public static bool isGameVersionChecked;
+        public static string gameVersionString;
+        public static string gameVersionReturnString;
 
-        public static bool verifyGameVersions(string myModGameVersion) {
-            currentGameVersion = "v0." + WorldManager.manageWorld.masterVersionNumber.ToString() + "." + WorldManager.manageWorld.versionNumber.ToString();
-            return myModGameVersion == currentGameVersion;
+        public static void verifyGameVersion(string myModGameVersion) {
+            if (!isGameVersionChecked) {
+                try {
+                    currentGameVersion = "v0." + WorldManager.manageWorld.masterVersionNumber.ToString() + "." + WorldManager.manageWorld.versionNumber.ToString();
+                }
+                catch (MissingReferenceException e) { }
+                if (myModGameVersion == currentGameVersion) {
+                    isGameVersionChecked = !isGameVersionChecked;
+                    gameVersionReturnString = "The game version's and mod's game version match.";
+                    
+                }
+                isGameVersionChecked = !isGameVersionChecked;
+                gameVersionReturnString=  "The game version's and mod's game version not match.";
+            }
         }
 
         /*public bool verifyModVersions(currentModVersion) {
