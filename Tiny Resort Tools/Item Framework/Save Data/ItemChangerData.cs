493d6e5dc25344966876474e7fa998afbe0fd691
Tanyrus <unbridledscholar@gmail.com>
2022-10-15 00:34:25 -0700

    Fix up comments

--- Tiny Resort Tools/Item Framework/Save Data/ItemChangerData.cs
+++ Tiny Resort Tools/Item Framework/Save Data/ItemChangerData.cs
@@ -14,41 +14,38 @@ namespace TinyResort {
         public int cycles;
         public bool startedUnderground;
         
-        public static void Save(int itemID, CurrentChanger changer) {
-            all.Add(new ItemChangerData { customItemID = TRItems.customItemsByItemID[itemID].customItemID, 
-                        objectXPos = changer.xPos, objectYPos = changer.yPos, counterSeconds = changer.counterSeconds, counterDays = changer.counterDays,
-                        timePerCycles = changer.timePerCycles, cycles = changer.cycles, startedUnderground = changer.startedUnderground, 
-                        houseXPos = changer.houseX, houseYPos = changer.houseY
-                    });
-
-            if (changer.houseX <= 0 && changer.houseY <= 0) {
-                WorldManager.manageWorld.onTileStatusMap[changer.xPos, changer.yPos] = -2;
-            }
-            else { HouseManager.manage.getHouseInfo(changer.houseX, changer.houseY).houseMapOnTileStatus[changer.xPos, changer.yPos] = -2; }
-            WorldManager.manageWorld.allChangers.Remove(changer);
-
-        }
-
-
         public static void LoadAll() {
             all = (List<ItemChangerData>)TRItems.Data.GetValue("ItemChangerData", new List<ItemChangerData>());
             TRTools.Log($"Loading ItemChangerData: {all.Count}");
             foreach (var item in all) { item.Load(); }
         }
 
+        public static void Save(int itemID, CurrentChanger changer) {
+            all.Add(
+                new ItemChangerData {
+                    customItemID = TRItems.customItemsByItemID[itemID].customItemID,
+                    objectXPos = changer.xPos,
+                    objectYPos = changer.yPos,
+                    counterSeconds = changer.counterSeconds,
+                    counterDays = changer.counterDays,
+                    timePerCycles = changer.timePerCycles,
+                    cycles = changer.cycles,
+                    startedUnderground = changer.startedUnderground,
+                    houseXPos = changer.houseX,
+                    houseYPos = changer.houseY
+                }
+            );
+
+            if (changer.houseX <= 0 && changer.houseY <= 0) { WorldManager.manageWorld.onTileStatusMap[changer.xPos, changer.yPos] = -2; }
+            else { HouseManager.manage.getHouseInfo(changer.houseX, changer.houseY).houseMapOnTileStatus[changer.xPos, changer.yPos] = -2; }
+            WorldManager.manageWorld.allChangers.Remove(changer);
+
+        }
+        
         public void Load() {
             if (!TRItems.customItems.TryGetValue(customItemID, out var customItem)) return;
-            TRTools.Log($"Before House Check: {houseXPos}, {houseYPos}");
-            if (houseXPos <= 0 && houseYPos <= 0) {
-                TRTools.Log($"Inside outside Check");
-
-                WorldManager.manageWorld.onTileStatusMap[objectXPos, objectYPos] = customItem.invItem.getItemId();
-            }
-            else {
-                TRTools.Log($"Before House restore");
-
-                HouseManager.manage.getHouseInfo(houseXPos, houseYPos).houseMapOnTileStatus[objectXPos, objectYPos] = customItem.invItem.getItemId();
-            }
+            if (houseXPos <= 0 && houseYPos <= 0) { WorldManager.manageWorld.onTileStatusMap[objectXPos, objectYPos] = customItem.invItem.getItemId(); }
+            else { HouseManager.manage.getHouseInfo(houseXPos, houseYPos).houseMapOnTileStatus[objectXPos, objectYPos] = customItem.invItem.getItemId();}
 
             CurrentChanger restoreChanger = new CurrentChanger(objectXPos, objectYPos);
             restoreChanger.cycles = cycles;
@@ -59,20 +56,5 @@ namespace TinyResort {
             restoreChanger.timePerCycles = timePerCycles;
             WorldManager.manageWorld.allChangers.Add(restoreChanger);
         }
-        /*
-        public static void Save(int tileType, int objectXPos, int objectYPos) {
-           all.Add(new ItemChangerData { customItemID = TRItems.customTileTypeByID[tileType].customItemID, objectXPos = objectXPos, objectYPos = objectYPos });
-           WorldManager.manageWorld.tileTypeMap[objectXPos, objectYPos] = 0;
-           WorldManager.manageWorld.refreshAllChunksInUse(objectXPos, objectYPos);
-
-        }
-
-        public void Load() {
-            if (!TRItems.customItems.TryGetValue(customItemID, out var customItem)) return; 
-            WorldManager.manageWorld.tileTypeMap[objectXPos, objectYPos] = customItem.invItem.placeableTileType;
-            WorldManager.manageWorld.refreshAllChunksInUse(objectXPos, objectYPos);
-        }*/
-
     }
-
 }
