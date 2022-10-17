fc196ddb6e4b0c1e6fd8e77f69ebb9041241b4c0
Tanyrus <unbridledscholar@gmail.com>
2022-10-16 22:45:25 -0700

    Signs and Clothing Mannequins

---
+++ Tiny Resort Tools/Item Framework/Save Data/ItemStatusData.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+
+namespace TinyResort {
+    
+    [Serializable]
+    internal class ItemStatusData : ItemSaveData {
+
+        public static List<ItemStatusData> all = new List<ItemStatusData>();
+        public static List<ItemStatusData> lostAndFound = new List<ItemStatusData>();
+
+        public static void LoadAll() {
+            lostAndFound = (List<ItemStatusData>)TRItems.Data.GetValue("ItemStatusDataLostAndFound", new List<ItemStatusData>());
+            TRTools.Log($"Loading ItemStatusData lostAndFound: {lostAndFound.Count}");
+
+            all = (List<ItemStatusData>)TRItems.Data.GetValue("ItemStatusData", new List<ItemStatusData>());
+            TRTools.Log($"Loading ItemStatusData: {all.Count}");
+           
+            foreach (var item in all) {
+                if (item.Load() == null) {
+                    if (!lostAndFound.Contains(item)) lostAndFound.Add(item);
+                }
+            }
+        }
+        
+        public static void Save(int itemID, int objectXPos, int objectYPos, int houseXPos, int houseYPos) {
+            all.Add(new ItemStatusData { customItemID = TRItems.customItemsByItemID[itemID].customItemID, objectXPos = objectXPos, objectYPos = objectYPos, houseXPos = houseXPos, houseYPos = houseYPos});
+            var houseDetails = houseXPos == -1 ? null : HouseManager.manage.getHouseInfo(houseXPos, houseYPos);
+
+            if (houseDetails != null) houseDetails.houseMapOnTileStatus[objectXPos, objectYPos] = -1; 
+            else WorldManager.manageWorld.onTileStatusMap[objectXPos, objectYPos] = -1; 
+        }
+        
+        public TRCustomItem Load() {
+            if (!TRItems.customItems.TryGetValue(customItemID, out var customItem)) return null;
+            var houseDetails = houseXPos == -1 ? null : HouseManager.manage.getHouseInfo(houseXPos, houseYPos);
+
+            if (houseDetails != null) houseDetails.houseMapOnTileStatus[objectXPos, objectYPos] = customItem.invItem.getItemId();
+            else WorldManager.manageWorld.onTileStatusMap[objectXPos, objectYPos] = customItem.invItem.getItemId(); 
+            return customItem;
+        }
+
+    }
+
+}
