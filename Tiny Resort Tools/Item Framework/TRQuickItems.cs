f4d8a97ddb923a58535d6c805dba5be76a1230ce
John Flaherty <slowcircuits@gmail.com>
2022-10-28 21:14:37 -0700

    Tweaked quick items

--- Tiny Resort Tools/Item Framework/TRQuickItems.cs
+++ Tiny Resort Tools/Item Framework/TRQuickItems.cs
@@ -17,15 +17,18 @@ namespace TinyResort {
 
     /// <summary>Tools for quickly creating clothing items.</summary>
     public class TRQuickItems {
-        internal static List<string> pathsArray = new List<string>();
+        
         internal static List<string> paths = new List<string>();
         internal static AssetBundle customClothingBundle = TRAssets.LoadAssetBundleFromDLL("clothing_bundle");
 
         internal static void FindAllPaths(string initialDir) {
             foreach (string file in Directory.GetFiles(initialDir)) {
                 if (Path.GetExtension(file) == ".qitem") {
-                    if (Path.GetFileNameWithoutExtension(file) == "items") { pathsArray.Add(file); }
-                    else { paths.Add(file); }
+                    //if (Path.GetFileNameWithoutExtension(file) == "items") { pathsArray.Add(file); }
+                    //else
+                    //{
+                    paths.Add(file);
+                    //}
                 }
             }
             foreach (string dir in Directory.GetDirectories(initialDir)) { FindAllPaths(dir); }
@@ -62,16 +65,16 @@ namespace TinyResort {
                 newItem.customItemID = "QI." + folderName + "_" + item.itemName.Replace(" ", "") + ext.Replace(".", "_");
                 TRItems.customItems[newItem.customItemID] = newItem;
             }
+            
             else if (!LeadPlugin.developerMode.Value && item.nexusID > 0) { 
                 newItem.customItemID = item.nexusID + "." + item.uniqueID;
                 TRItems.customItems[newItem.customItemID] = newItem;
             }
-            else {
-                TRTools.LogError($"Loading a Quick Item in with a -1 Nexus ID. THis is not allowed and will be blocked. If you are a developer, please turn on developer mode.");
-            }
+            
+            else { TRTools.LogError($"Loading a Quick Item in with a -1 Nexus ID. THis is not allowed and will be blocked. If you are a developer, please turn on developer mode."); }
             
             TRTools.Log($"Custom ID: {newItem.customItemID}");
-            newItem.inventoryItem.value = item.itemValue;
+            newItem.inventoryItem.value = item.value;
 
         }
 
@@ -80,32 +83,14 @@ namespace TinyResort {
             var oneItem = QuickItems.CreateFromJson(json);
 
             string relativePath = path.Remove(0, Paths.PluginPath.Length).Replace(Path.GetFileName(path), oneItem.fileName);
-            InitializeCustomItem(relativePath, customClothingBundle.LoadAsset<GameObject>(oneItem.itemType), oneItem);
+            InitializeCustomItem(relativePath, customClothingBundle.LoadAsset<GameObject>(oneItem.type), oneItem);
 
-            TRTools.Log($"Single Item: {oneItem.fileName} | {oneItem.itemName} | {oneItem.itemType} | {oneItem.itemValue}");
-        }
-
-        // This doesn't work yet...
-        internal static void LoadArrayItem(string path) {
-            string json = File.ReadAllText(path);
-            var allItems = JsonUtility.FromJson<RootQuickItems>(json);
-
-            try {
-                TRTools.LogError($"Paths Array Size: {allItems.items.Count}");
-                foreach (var item in allItems.items) {
-                    string relativePath = path.Remove(0, Paths.PluginPath.Length).Replace(Path.GetFileName(path), item.fileName);
-                    TRTools.LogError($"Multilisted Item: {item.fileName} | {item.itemName} | {item.itemType} | {item.itemValue}");
-                    //InitializeCustomItem(relativePath, customClothingBundle.LoadAsset<GameObject>(item.itemType), item);
-                }
-            }
-            catch { TRTools.LogError($"MultiItems Failed"); }
+            TRTools.Log($"Single Item: {oneItem.fileName} | {oneItem.itemName} | {oneItem.type} | {oneItem.value}");
         }
 
         internal static void LoadAllQuickItems() {
             FindAllPaths(Paths.PluginPath);
-
             foreach (var item in paths) { LoadItem(item); }
-            foreach (var items in pathsArray) { LoadArrayItem(items); }
         }
     }
 
@@ -114,68 +99,13 @@ namespace TinyResort {
 
         public int nexusID;
         public int uniqueID;
-        public string fileName;
-        public int itemValue;
         public string itemName;
-        public string itemType;
+        public string fileName;
+        public int value;
+        public string type;
 
         public static QuickItems CreateFromJson(string jsonString) { return JsonUtility.FromJson<QuickItems>(jsonString); }
 
     }
 
-    [Serializable]
-    public class RootQuickItems {
-        public List<QuickItems> items = new List<QuickItems>();
-    }
-
-    public static class JsonHelper {
-        public static T[] FromJson<T>(string json) {
-            Wrapper<T> wrapper = JsonUtility.FromJson<Wrapper<T>>(json);
-            return wrapper.Items;
-        }
-
-        public static string ToJson<T>(T[] array) {
-            Wrapper<T> wrapper = new Wrapper<T>();
-            wrapper.Items = array;
-            return JsonUtility.ToJson(wrapper);
-        }
-
-        public static string ToJson<T>(T[] array, bool prettyPrint) {
-            Wrapper<T> wrapper = new Wrapper<T>();
-            wrapper.Items = array;
-            return JsonUtility.ToJson(wrapper, prettyPrint);
-        }
-
-        [Serializable]
-        public class Wrapper<T> {
-            public T[] Items;
-        }
-    }
-
 }
-
-/*internal static void MainMenuInitialization() {
-    foreach (var item in TRItems.customItems) {
-        if (item.Key.Contains("QI.")) {
-            //item.Value.invItem;
-            //TRItems.customItemsByItemID[]
-        }
-    }
-}
-         
-            
-            /*
-            foreach (var path in paths) { TRTools.Log(path); }
-            
-            string filePath = Path.Combine(Paths.PluginPath, "TR Tools", "quick_items");
-            
-            string json1 = File.ReadAllText(Path.Combine(filePath, "quickitems.qitem"));
-            TRTools.Log(json1);
-
-            var QIArray = JsonUtility.FromJson<RootQuickItems>(json1);
-            
-            TRTools.Log($"Starting Check...");
-            TRTools.Log($"Multi...: QI Array: {QIArray}");
-            TRTools.Log($"Multi...: QI Array: {QIArray.items}");
-            TRTools.Log($"Multi...: QI Array: {QIArray.items.Count}");  
-            */
--- Tiny Resort Tools/Mod Management/LeadPlugin.cs
+++ Tiny Resort Tools/Mod Management/LeadPlugin.cs
@@ -20,7 +20,7 @@ namespace TinyResort {
         
         public const string pluginName = "TRTools";
         public const string pluginGuid = "dev.TinyResort." + pluginName;
-        public const string pluginVersion = "0.7.1";
+        public const string pluginVersion = "0.7.3";
 
         private void Awake() {
 
